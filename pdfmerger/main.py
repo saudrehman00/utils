0b97f4fcaf3d347aea557fb4e80aaae014805e8c
Saud Rehman <saudrehman00@hotmail.com>
2023-02-05 19:04:26 -0500

    Fix pdf merger

--- pdfmerger/main.py
+++ pdfmerger/main.py
@@ -2,19 +2,17 @@
 import PyPDF2
 import os
 
-def merge_pdfs(pdfs, output):
-    # Create a PDF object to store the merged PDF
-    merger = PyPDF2.PdfFileMerger()
-    
-    # Loop through each PDF in the list
+import PyPDF2 
+
+def merge_pdfs(pdfs,output):
+
+    mergeFile = PyPDF2.PdfFileMerger()
+
     for pdf in pdfs:
-        # Open each PDF and add it to the merger
-        with open(pdf, 'rb') as file:
-            merger.append(file)
-    
-    # Write the merged PDF to the output file
-    with open(output, 'wb') as file:
-        merger.write(file)
+
+        mergeFile.append(PyPDF2.PdfFileReader(pdf, 'rb'))
+
+    mergeFile.write(output)
 
 # Example usage
 pdfs = [file for file in os.listdir() if file.endswith('.pdf')]
